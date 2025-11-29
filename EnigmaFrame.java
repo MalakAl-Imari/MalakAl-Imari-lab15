c0fc1b33c78470fa7efa3c7c90fe934e49df356b
MalakAl-Imari <malakammar211@gmail.com>
2025-11-29 14:42:34 -0500

    GUI parts implemented

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,3 +1,22 @@
-public class EnigmaFrame {
-    
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class EnigmaFrame extends JFrame {
+
+private JComboBox<Integer> innerRotor;
+private JComboBox<Integer> middleRotor;
+private JComboBox<Integer> outerRotor;
+
+private JTextField initialStartingchars;
+
+private JButton encryptButton;
+private JButton decryptButton;
+
+private JTextArea inputArea;
+private JTextArea outputArea;
+
+
 }
