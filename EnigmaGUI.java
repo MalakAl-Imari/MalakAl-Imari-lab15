ce7eb2a8e8c53b559b28b7d06b18b3665044bbdd
MalakAl-Imari <malakammar211@gmail.com>
2025-11-29 14:30:57 -0500

    EnigmaGUI implemented

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
 public class EnigmaGUI {
-    
+  public static void main(String[] args) {
+        EnigmaFrame frame = new EnigmaFrame();
+        frame.setTitle("Enigma GUI");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //end program when you click x
+        frame.setVisible(true);
+    }  
 }
