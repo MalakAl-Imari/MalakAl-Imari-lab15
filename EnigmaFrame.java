9f438c9199a5a1e079a05b50d7ea10efb8990f19
MalakAl-Imari <malakammar211@gmail.com>
2025-11-29 19:53:20 -0500

    final edits

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,11 +1,6 @@
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class EnigmaFrame extends JFrame {
 
@@ -23,14 +18,15 @@ private JTextArea outputArea;
 
 
 public EnigmaFrame() {
-       
+setTitle("Enigma GUI");  
+
 Integer[] rotorChoices = {1, 2, 3, 4, 5};
 
-innerCombo  = new JComboBox<>(rotorChoices);
-middleCombo = new JComboBox<>(rotorChoices);
-outerCombo  = new JComboBox<>(rotorChoices);
+innerRotor  = new JComboBox<>(rotorChoices);
+middleRotor = new JComboBox<>(rotorChoices);
+outerRotor  = new JComboBox<>(rotorChoices);
 
-initialPositionsField = new JTextField("", 3);
+initialStartingchars = new JTextField("", 3);
 
 encryptButton = new JButton("Encrypt");
 decryptButton = new JButton("Decrypt");
@@ -40,20 +36,20 @@ outputArea = new JTextArea(4, 40);
 outputArea.setEditable(false);
 
 
-//top part
+//top panel
 JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
 topPanel.add(new JLabel("Inner"));
-topPanel.add(innerCombo);
+topPanel.add(innerRotor);
 
 topPanel.add(new JLabel("Middle"));
-topPanel.add(middleCombo);
+topPanel.add(middleRotor);
 
 topPanel.add(new JLabel("Out"));
-topPanel.add(outerCombo);
+topPanel.add(outerRotor);
 
 topPanel.add(new JLabel("Initial Positions"));
-topPanel.add(initialPositionsField);
+topPanel.add(initialStartingchars);
 
 topPanel.add(encryptButton);
 topPanel.add(decryptButton);
@@ -88,8 +84,45 @@ pack();
     }
 
 private class EnigmaActionListener implements ActionListener {
+    
     public void actionPerformed(ActionEvent e) {
+            // get rotor choices
+            int inner  = (Integer) innerRotor.getSelectedItem();
+            int middle = (Integer) middleRotor.getSelectedItem();
+            int outer  = (Integer) outerRotor.getSelectedItem();
 
+            // read inputted positions 
+            String position = initialStartingchars.getText();
+            if (position.length() != 3) {
+            return;
+            }
+
+            // get input 
+            String message = inputArea.getText();
+
+            // encrypt or decrypt 
+            String button = e.getActionCommand(); 
+            String option;
+            if (button.equals("Encrypt")) {
+                option = "encrypt";
+            } else {
+                option = "decrypt";
+            }
+
+            // build args 
+            String[] args = new String[6];
+            args[0] = String.valueOf(inner);
+            args[1] = String.valueOf(middle);
+            args[2] = String.valueOf(outer);
+            args[3] = position;
+            args[4] = option;
+            args[5] = message;
+
+            // call enigma/Comms
+            String result = Comms.run(args);
+
+            // return output result
+            outputArea.setText(result);
 
 
 
